353daaa3b4d6cff100a12fc95007dd5c58027804
adamnoszczynski <adam.noszczynski@gmail.com>
2022-09-24 01:18:16 +0200

    implement Cart methods

--- week-1-ts-ecosystem/w01e03-object-oriented-structure/src/Cart.ts
+++ week-1-ts-ecosystem/w01e03-object-oriented-structure/src/Cart.ts
@@ -1,10 +1,54 @@
-import { ProductType } from "./Product";
+import Product, {ProductId} from "./Product";
+import ProductForFree from "./ProductForFree";
+
+type Products = Map<ProductId, Product>;
 
 class Cart {
-    productType: ProductType;
+    #products: Products = new Map();
 
-    constructor(productType: ProductType) {
-        this.productType = productType;
+    constructor() {
+        //
+    }
+
+    addProduct (product: Product): void {
+        this.products.set(product.id, product);
+    }
+
+    getProduct (productId: ProductId): void {
+        this.products.get(productId);
+    }
+
+    updateProduct (product: Product): void {
+        this.products.set(product.id, product);
+    }
+
+    deleteProduct (productId: ProductId): void {
+        this.products.delete(productId);
+    }
+
+    get products (): Products {
+        return this.#products
+    }
+
+    countProducts (): number {
+        return this.products.size;
+    }
+
+    sumProducts (): number {
+        const arr: [string, Product][] = Array.from(this.products);
+
+        return arr.reduce((sum, [, product]) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            if (product instanceof ProductForFree || !product.price) {
+                return sum;
+            }
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            sum += product.price * product.count;
+            return sum;
+        }, 0);
     }
 }
 

c3d7049107e6b2921771ec5968545c747903a91f
adamnoszczynski <adam.noszczynski@gmail.com>
2022-09-26 20:02:19 +0200

    fix https://github.com/michaljabi/js-na-frontach-zadania/pull/16#discussion_r980047065

--- week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
@@ -16,7 +16,7 @@ function hasGivenAge(requiredAge: number): (user: User) => boolean {
   return (user) => (user.age >= requiredAge)
 }
 
-const isAdult = hasGivenAge(1)
+const isAdult = hasGivenAge(18)
 
 console.log(`User ${user.name} is ${isAdult(user) ? 'adult' : 'minor'}`)
 console.log(`and has${hasAddress(user) ? '' : ' no'} address`)
